f79213f0058c692133aaa28b533760fef2a76484
unknown <pateldeepanshu196@gmail.com>
2024-04-21 10:05:44 +0530

    ENV change

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
